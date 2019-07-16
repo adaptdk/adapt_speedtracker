ce72cb68af7d42d3348a431d7caeab74d6c98940
Casper Juel <casper@adapt.dk>
2019-07-16 15:43:45 +0200

    PIKIP-3 Updating webpack and adding livereload

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,33 @@
-'use strict'
+const webpack = require('webpack');
+const path = require('path');
 
-const webpack = require('webpack')
-const path = require('path')
-
-let config = {
-  entry: [
-    'whatwg-fetch',
-    './app/App'
-  ],
+const config = {
+  entry: './app/App.js',
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        loader: 'babel-loader',
-        exclude: /node_modules/,
         test: /\.(js|jsx)$/,
-        query: {
-          presets: ['es2015', 'es2016', 'react'],
-          plugins: [
-            'transform-es2015-arrow-functions',
-            'transform-class-properties'
-          ]
-        }
+        use: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  devServer: {
+    proxy: {
+      '*': 'http://0.0.0.0:4820/',
+    },
   }
 }
 
-if (process.env.NODE_ENV === 'production') {
-  config.plugins = [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: true
-      }
-    })
-  ]
-}
-
-module.exports = config
+module.exports = config;
