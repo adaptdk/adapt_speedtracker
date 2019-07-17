2d8c51dd0359f2a3437a57725776572872049d04
Casper Juel <casper@adapt.dk>
2019-07-17 11:27:48 +0200

    PIKIP-5 updating scss structure and add eslint

--- app/App.js
+++ app/App.js
@@ -16,7 +16,7 @@ const parseUrl = require('query-string').parse
 require('es6-promise').polyfill()
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     let activeProfile = window.PROFILES.find(profile => profile.active)
 
@@ -106,13 +106,12 @@ class App extends React.Component {
     })
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const dateRange = Utils.getDateRangeForPeriod(this.state.period)
-
     this._fetchData(dateRange.from, dateRange.to)
   }
 
-  componentDidUpdate (oldProps, oldState) {
+  componentDidUpdate(oldProps, oldState) {
     if ((oldState.period !== this.state.period) || (oldState.profile !== this.state.profile)) {
       const dateRange = Utils.getDateRangeForPeriod(this.state.period)
 
@@ -120,9 +119,8 @@ class App extends React.Component {
     }
   }
 
-  render () {
+  render() {
     const { state } = this;
-    console.log(brandStyling);
 
     return (
       <div style={brandStyling}>
@@ -132,17 +130,17 @@ class App extends React.Component {
           onProfileChange={this._changeProfile}
         />
 
-          {this.state.loading ? <Loader /> :
-            <Dashboard
-              {...state}
-              onPeriodChange={this._changePeriod}
-              onProfileChange={this._changeProfile}
-            />
-          }
+        {this.state.loading ? <Loader /> :
+          <Dashboard
+            {...state}
+            onPeriodChange={this._changePeriod}
+            onProfileChange={this._changeProfile}
+          />
+        }
         <Footer />
       </div>
     )
   }
 }
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'));
