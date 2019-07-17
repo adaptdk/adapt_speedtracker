3e156d689ccb228aee27051b0df1ab864ffafc04
Casper Juel <casper@adapt.dk>
2019-07-17 13:37:18 +0200

    PIKIP-5 converting to functional components and updating brand.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
-const webpack = require('webpack');
-const path = require('path');
-
 const config = {
   entry: './app/App.js',
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
     extensions: [
       '.js',
-      '.jsx'
-    ]
+      '.jsx',
+    ],
   },
   devServer: {
     proxy: {
       '*': 'http://0.0.0.0:4820/',
     },
-  }
-}
+  },
+};
 
 module.exports = config;
