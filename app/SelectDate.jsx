1b14b1b7ffcd546610616ff0c31366a64742d247
Rokas Kasperavicius <rokaskasperavicius@Rokass-MBP.lbg.adapt.dk>
2019-08-06 08:52:13 +0200

    PIKIP-8 Finished updating datepicker styling

--- app/SelectDate.jsx
+++ app/SelectDate.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
+import SVG from 'react-inlinesvg';
+import CalenderTo from './icons/CalenderTo.svg';
+import CalenderFrom from './icons/CalenderFrom.svg';
+
 import { initialDate } from './Constants';
 
 const SelectDate = ({ onPeriodChange }) => {
@@ -21,25 +25,33 @@ const SelectDate = ({ onPeriodChange }) => {
 
   return (
     <div className="c-Datepicker">
-      <DatePicker
-        dateFormat="dd MM yyyy"
-        selected={from}
-        selectsStart
-        startDate={from}
-        endDate={to}
-        onChange={onStartChange}
-        maxDate={to}
-      />
-      <DatePicker
-        dateFormat="dd MM yyyy"
-        selected={to}
-        selectsEnd
-        startDate={from}
-        endDate={to}
-        onChange={onEndChange}
-        minDate={from}
-        maxDate={new Date()}
-      />
+      <section>
+        <SVG width="20px" src={CalenderFrom} />
+        <DatePicker
+          className="c-Datepicker--from"
+          dateFormat="dd MM yyyy"
+          selected={from}
+          selectsStart
+          startDate={from}
+          endDate={to}
+          onChange={onStartChange}
+          maxDate={to}
+        />
+      </section>
+      <section>
+        <SVG width="20px" src={CalenderTo} />
+        <DatePicker
+          className="c-Datepicker--to"
+          dateFormat="dd MM yyyy"
+          selected={to}
+          selectsEnd
+          startDate={from}
+          endDate={to}
+          onChange={onEndChange}
+          minDate={from}
+          maxDate={new Date()}
+        />
+      </section>
     </div>
   );
 };
